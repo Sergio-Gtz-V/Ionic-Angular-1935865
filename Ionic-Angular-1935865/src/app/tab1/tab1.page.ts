4fd7fcd30f7db75dca84a5fe911f524aacdffbbd
Sergio-Gtz-V <Sergio-Gtz-V@github.com>
2022-05-27 01:26:54 -0500

    Practica 4

--- Ionic-Angular-1935865/src/app/tab1/tab1.page.ts
+++ Ionic-Angular-1935865/src/app/tab1/tab1.page.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnChanges{
 
   constructor() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    
+  }
 
   alumnos = [
     {
@@ -96,4 +100,35 @@ export class Tab1Page {
       "matricula": "ABC123"
     }
   ];
+
+  @Input() nombre: string = "";
+  @Input() apellido: string = "";
+  @Input() matricula: string = "";
+
+
+  
+  agregarAlumno(): void {
+    var nuevoAlumno: any = {
+      "nombre": this.nombre,
+      "apellido": this.apellido,
+      "matricula": this.matricula
+    }
+
+    this.alumnos.push(nuevoAlumno); //Agrega elemento al arreglo
+
+   this.alumnos.sort(function(a, b) {
+    var nameA = a.nombre.toUpperCase(); 
+    var nameB = b.nombre.toUpperCase(); 
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+  
+    return 0;
+  });
+
+  console.log(this.alumnos)
+  }
 }
